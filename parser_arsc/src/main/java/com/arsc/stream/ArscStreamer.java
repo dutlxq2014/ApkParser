2eb6aed2b49c359143d67c14922ca16ace7592b7
xueqiu.lxq <xueqiu.lxq@alibaba-inc.com>
2017-07-25 00:25:58 +0800

    init arsc module

---
+++ parser_arsc/src/main/java/com/arsc/stream/ArscStreamer.java
@@ -0,0 +1,12 @@
+package com.arsc.stream;
+
+import com.common.stream.BaseStreamer;
+
+/**
+ *
+ * Created by xueqiulxq on 25/07/2017.
+ */
+
+public class ArscStreamer extends BaseStreamer {
+
+}
---
+++ parser_arsc/src/main/java/com/arsc/stream/LittleEndianStreamer.java
@@ -0,0 +1,14 @@
+package com.arsc.stream;
+
+/**
+ *
+ * Created by xueqiulxq on 25/07/2017.
+ */
+
+public class LittleEndianStreamer extends ArscStreamer {
+
+    public LittleEndianStreamer() {
+        super();
+    }
+
+}
