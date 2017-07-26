8c532b051c48fcea915cc57ffaef5e6a2205f983
dutlxq2014 <dutlxq2014@163.com>
2017-07-27 01:09:23 +0800

    parse arsc string pool chunk

--- parser_arsc/src/main/java/com/arsc/data/ResStringPoolChunk.java
+++ parser_arsc/src/main/java/com/arsc/data/ResStringPoolChunk.java
@@ -3,6 +3,9 @@ package com.arsc.data;
 import com.arsc.stream.ArscStreamer;
 import com.common.PrintUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * Created by xueqiulxq on 26/07/2017.
@@ -17,9 +20,12 @@ public class ResStringPoolChunk {
     public long stringCount;
     public long styleCount;
     public long flags;
-    public long stringsStart;
+    public long stringsStart;           // Offset from this chunk starting.
     public long stylesStart;
-
+    public long stringOffsetArray[];    // Offset from string pool. The first one is 0x00000000
+    public long styleOffsetArray[];
+    public List<String> strings;
+    public List<String> styles;
 
     public static ResStringPoolChunk parseFrom(ArscStreamer s) {
 
@@ -30,6 +36,25 @@ public class ResStringPoolChunk {
         chunk.flags = s.readUInt();
         chunk.stringsStart = s.readUInt();
         chunk.stylesStart = s.readUInt();
+        long[] strOffsets = chunk.stringOffsetArray = new long[(int) chunk.stringCount];
+        long[] styleOffsets = chunk.styleOffsetArray = new long[(int) chunk.styleCount];
+        List<String> strings = chunk.strings = new ArrayList<String>((int) chunk.stringCount);
+        List<String> styles = chunk.styles = new ArrayList<String>((int) chunk.styleCount);
+
+        for (int i=0; i<chunk.stringCount; ++i) {
+            strOffsets[i] = s.readUInt();
+        }
+        for (int i=0; i<chunk.styleCount; ++i) {
+            styleOffsets[i] = s.readUInt();
+        }
+        for (int i=0; i<chunk.stringCount; ++i) {
+            int len = (s.readUShort() & 0x7f00) >> 8;
+            String str = s.readNullEndString(len + 1); // The last byte is 0x00
+            strings.add(str);
+        }
+        for (int i=0; i<chunk.styleCount; ++i) {
+
+        }
 
         return chunk;
     }
@@ -48,6 +73,28 @@ public class ResStringPoolChunk {
                 .append("  ").append("sorted=").append((flags & SORTED_FLAG) != 0 ? "Y" : "N").append('\n');
         builder.append(String.format(form, "stringsStart", PrintUtil.hex4(stringsStart)));
         builder.append(String.format(form, "stylesStart", PrintUtil.hex4(stylesStart)));
+
+        builder.append("\nString offset array:\n");
+        for (int i=0; i<stringOffsetArray.length; ++i) {
+            builder.append(PrintUtil.hex4(stringOffsetArray[i])).append(" ");
+            if ((i+1) % 16 == 0) {
+                builder.append('\n');
+            }
+        }
+
+        builder.append("\nStyle offset array:\n");
+        for (int i=0; i<styleOffsetArray.length; ++i) {
+            builder.append(PrintUtil.hex4(styleOffsetArray[i])).append(" ");
+        }
+
+        builder.append("\nStringPool Content:\n");
+        for (int i=0; i<stringCount; ++i) {
+            builder.append(String.format("0x%x: ", i+1)).append(strings.get(i)).append('\n');
+        }
+        builder.append("\nStylePools Content:\n");
+        for (int i=0; i<styleCount; ++i) {
+            builder.append(styles.get(i)).append("\n");
+        }
 
         return builder.toString();
     }
--- parser_arsc/src/main/java/com/arsc/stream/ArscStreamer.java
+++ parser_arsc/src/main/java/com/arsc/stream/ArscStreamer.java
@@ -2,6 +2,8 @@ package com.arsc.stream;
 
 import com.common.stream.BaseStreamer;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  *
  * Created by xueqiulxq on 25/07/2017.
@@ -16,4 +18,14 @@ public abstract class ArscStreamer extends BaseStreamer {
     public abstract char readChar16();
 
     public abstract char readChar8();
+
+    public String readNullEndString(int lenInclEnd) {
+        byte[] bytes = super.read(lenInclEnd);
+        try {
+            return new String(bytes, 0, lenInclEnd-1, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return new String(bytes, 0, lenInclEnd-1);
+        }
+    }
 }
